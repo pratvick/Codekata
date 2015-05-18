6cfa1d0f0415391760037b6cda30410e4ddc8a28
Prateek <prateek.kh@directi.com>
2015-05-18 19:11:33 +0530

    Separate two questions binary search and data munging

--- BinarySearch/BinarySearch/main.cpp
+++ BinarySearch/BinarySearch/main.cpp
@@ -59,73 +59,16 @@ int binarySearchRecursive1(int target, int *a, int n, int low) {
   }
 }
 
-void print(vector<string> v) {
-  for (int i = 0; i < v.size(); i++) {
-    cout << v[i] << " ";
+int main () {
+  int n, target;
+  cin >> n;
+  int a[n];
+  for (int i = 0; i < n; i++) {
+    cin >> a[i];
   }
-  cout << endl;
-}
-
-vector <string> particularColumn(string columnHeading, string filePath) {
-  int i, col;
-  vector <string> v;
-  ifstream file;
-  file.open(filePath);
-  string line;
-  string value;
-  getline(file, line);
-  col = int(line.find(columnHeading));
-  while (getline(file, line)) {
-    i = col;
-    value = "";
-    while ((i < line.length()) && !((line[i] >= '0' && line[i] <= '9') || (line[i] >= 'a' && line[i] <= 'z') || (line[i] >= 'A' && line[i] <= 'Z') || (line[i] == '_'))) {
-      i++;
-    }
-    while((i < line.length()) && ((line[i] >= '0' && line[i] <= '9') || (line[i] >= 'a' && line[i] <= 'z') || (line[i] >= 'A' && line[i] <= 'Z') || (line[i] == '_'))) {
-      value += line[i];
-      i++;
-    }
-    if (value != "") {
-      v.push_back(value);
-    }
-  }
-  return v;
-}
-
-int convertStringIntoInteger(string s) {
-  int number = 0;
-  for (int i = 0; i < s.size(); i++) {
-    if (s[i] >= '0' && s[i] <= '9') {
-      number *= 10;
-      number += s[i] - '0';
-    }
-  }
-  return number;
-}
-
-int minDiffIndex(vector<string> v1, vector<string> v2, int low, int high) {
-  int min = INT32_MAX, minIndex = 0;
-  for (int i = low; i <= high; i++) {
-    if (min > abs(convertStringIntoInteger(v1[i]) - convertStringIntoInteger(v2[i]))) {
-      min = abs(convertStringIntoInteger(v1[i]) - convertStringIntoInteger(v2[i]));
-      minIndex = i;
-    }
-  }
-  return minIndex;
-}
-
-int main() {
-  vector <string> v1, v2, names, day;
-
-  v1 = particularColumn("MxT", "weather.txt");
-  v2 = particularColumn("MnT", "weather.txt");
-  day = particularColumn("Dy", "weather.txt");
-  cout << day[minDiffIndex(v1, v2, 0, int(v1.size()) - 2)] << endl;
-
-  names = particularColumn("Team", "football.txt");
-  v1 = particularColumn("F", "football.txt");
-  v2 = particularColumn("A", "football.txt");
-  cout << names[minDiffIndex(v1, v2, 0, int(v1.size()) - 1)] << endl;
-
+  cin >> target;
+  cout << binarySearchIterative(target, a, n) << endl;
+  cout << binarySearchRecursive(target, a, n) << endl;
+  cout << binarySearchRecursive1(target, a, n, 0) << endl;
   return 0;
 }
